4b97635e1af9149aa6855a24c97a4bb535fb72a2
Hamza Mateen <43349591+HamzaMateen@users.noreply.github.com>
2022-09-01 03:17:20 +0000

    some new functions

--- Main.hs
+++ Main.hs
@@ -4,14 +4,45 @@ doubleMe x = x + x
 
 minmax a b = min a (max a b)
 
-doubleSmallerNumber x = if x > 100
-                        then x 
-                        else 2 * x
+doubleSmallerNumber x =
+  if x > 100
+    then x
+    else 2 * x
 
-doubleSmallerNumber' x = (if x > 100 then x else x * 2 ) + 1
+doubleSmallerNumber' x = (if x > 100 then x else x * 2) + 1
+
+-- lists and more functions
+addedLists = [1 .. 4] ++ [9 .. 12]
+
+woot = ['w', 'o'] ++ ['o', 't']
+
+aSmallCat = 'A' : " Small Cat"
+
+addANumber = 5 : [1 .. 5]
+
+sixthChar = "Mateen" !! 5
+
+listOfLists =
+  [ [1 .. 5],
+    [6 .. 10],
+    [11 .. 15]
+  ]
 
 main = do
+  -- lists, ranges  and more functions
+  print addedLists
+  print woot
+  print aSmallCat
+  print addANumber
+  print sixthChar
+  print (head listOfLists)
+  print (last listOfLists)
+  print (length (init (reverse listOfLists)))
+  print (product (head listOfLists))
+  print (elem 4 [1..10])
+  -- first steps
+  print (null [123])
   putStrLn "Hello"
   putStrLn "World"
 
-  print ( doubleSmallerNumber' 5 )
+  print (doubleSmallerNumber' 5)
